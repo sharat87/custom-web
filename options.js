c2fa26c87da58610818aaab1d08873927376efe8
Shrikant Sharat <shrikantsharat.k@gmail.com>
2013-12-28 16:10:30 +0530

    CodeMirror yay!

--- options.js
+++ options.js
@@ -1,10 +1,7 @@
 function loadDomains(callback) {
     chrome.storage.sync.get(null, function (data) {
-        var domains = [];
-        for (var key in data)
-            domains.push(key);
         $('#domain-list').html(Mustache.render($('#domain-list-tpl').text(), {
-            domains: domains
+            domains: Object.keys(data)
         }));
         if (callback) callback();
     });
@@ -16,8 +13,9 @@ function applyHash() {
         .find('a[href="#' + currentHost + '"]').addClass('active');
     chrome.storage.sync.get(currentHost, function (data) {
         if (!data[currentHost]) return;
-        cssInput.val(data[currentHost].css || '');
-        jsInput.val(data[currentHost].js || '');
+        cssInput.setValue(data[currentHost].css || '');
+        jsInput.setValue(data[currentHost].js || '');
+        cssInput.focus();
     });
     if (currentHost == '!default')
         deleteBtn.attr({disabled: ''});
@@ -27,30 +25,38 @@ function applyHash() {
 
 function save() {
     var data = {};
-    data[currentHost] = {
-        css: cssInput.val(),
-        js: jsInput.val()
-    };
+    data[currentHost] = {css: cssInput.getValue(), js: jsInput.getValue()};
     chrome.storage.sync.set(data);
 }
 
 function deleteCurrent() {
     chrome.storage.sync.remove(currentHost, function () {
         loadDomains(function () {
-            // $('#domain-list a[href="#' + currentHost + '"]').remove();
             location.hash = '#!default';
         });
     });
 }
 
+$.fn.CodeMirror = function (mode) {
+    return CodeMirror(this[0], {
+        mode: mode,
+        lineNumbers: true,
+        showCursorWhenSelecting: true,
+        styleActiveLine: true,
+        matchBrackets: true,
+        autoCloseBrackets: '(){}[]\'\'""'
+    });
+};
+
 var currentHost,
-    cssInput = $('#css'),
-    jsInput = $('#js'),
-    deleteBtn = $('#del-btn');
+    deleteBtn = $('#del-btn'),
+    cssInput = $('#css-input').CodeMirror('css'),
+    jsInput = $('#js-input').CodeMirror('javascript');
 
 deleteBtn.on('click', deleteCurrent);
 $(window).on('hashchange', applyHash);
-$('textarea').on({keyup: save, change: save});
+cssInput.on('change', save);
+jsInput.on('change', save);
 
 loadDomains(function () {
     if (location.hash)
