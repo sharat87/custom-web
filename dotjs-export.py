38e0156bc4a967314fa81279a99b710a82a65287
Shrikant Sharat <shrikantsharat.k@gmail.com>
2013-12-31 13:48:04 +0530

    Added a script to help migrate from dotjs.

---
+++ dotjs-export.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+'''
+Exports your dotjs scripts (in ~/.js) and styles (in ~/.css) to a format
+that can be imported by Custom Web. See https://github.com/sharat87/custom-web.
+
+The exported data is printed to stdout.
+'''
+
+import os
+from glob import glob
+from collections import defaultdict
+from itertools import chain
+import json
+
+data = defaultdict(lambda: {'css': '', 'js': ''})
+patterns = ['~/.js/*.js', '~/.css/*.css']
+
+for fname in chain(*(glob(os.path.expanduser(pat)) for pat in patterns)):
+    domain, ext = os.path.splitext(os.path.basename(fname))
+    with open(fname) as f:
+        data[domain][ext[1:]] = f.read()
+
+print(json.dumps({'sync': data}, indent=4, separators=(',', ': '), sort_keys=True))
